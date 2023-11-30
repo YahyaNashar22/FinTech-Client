9cb32f3182484f346885fff5122d9f4dcf22b23b
Rachwan <rachwan.harb2023@gmail.com>
2023-11-30 20:17:56 +0200

    Applied Regex for input validation

--- client/src/pages/EditCompany/EditCompany.js
+++ client/src/pages/EditCompany/EditCompany.js
@@ -1,19 +1,88 @@
-import React from "react";
+import {React, useState} from "react";
 import style from './EditCompany.module.css';
 
 const EditCompany = () => {
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const validateName = (name) => {
+    const nameRegex = /^[A-Za-z\s]+$/;
+    return name === null || name.trim() === '' || nameRegex.test(name);
+  };
+
+  const validateCapital = (capital) => {
+    const capitalRegex = /^\d+$/;
+    return capital === null || capital.trim() === '' || capitalRegex.test(capital);
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return email === null || email.trim() === '' || emailRegex.test(email);
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    const phoneRegex = /^\d+$/;
+    return phoneNumber === null || phoneNumber.trim() === '' || phoneRegex.test(phoneNumber);
+  };
+
+  const validateWebsite = (website) => {
+    const websiteRegex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+    return website === null || website.trim() === '' || websiteRegex.test(website);
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const formData = new FormData(event.target);
+  
+    const Name = formData.get('Name');
+    const Capital = formData.get('Capital');
+    const Email = formData.get('Email');
+    const PhoneNumber = formData.get('PhoneNumber');
+    const Website = formData.get('Website');
+
+    const errors = {};
+
+    if (!validateName(Name)) {
+      errors.Name = 'Invalid name';
+    }
+
+    if (!validateCapital(Capital)) {
+      errors.Capital = 'Invalid capital';
+    }
+
+    if (!validateEmail(Email)) {
+      errors.Email = 'Invalid email';
+    }
+  
+    if (!validatePhoneNumber(PhoneNumber)) {
+      errors.PhoneNumber = 'Invalid phone number';
+    }
+
+    if (!validateWebsite(Website)) {
+      errors.Website = 'Invalid website URL';
+    }
+
+    setFormErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      // API call to update the company information
+    }
+  };
+
   return (
     <main className={style.main}>
       <div className={style.header}>
-        <h1 className={style.title}>Company Information:</h1>
+        <h1 className={style.title}>Company Information</h1>
       </div>
-      <form className={style.form} action="" method="GET">
+      <form className={style.form} onSubmit={handleSubmit}>
         <div className={style.input}>
           <label htmlFor="nameLabel" className={style.label}>
             Name:
           </label>
           <input type="text" name="Name" id="nameLabel" />
         </div>
+        {formErrors.Name && <span className={style.error}>{formErrors.Name}</span>}
         <div className={style.input}>
           <label htmlFor="descriptionLabel" className={style.label}>
             Description:
@@ -26,6 +95,14 @@ const EditCompany = () => {
           </label>
           <input type="text" name="Capital" id="capitalLabel"/>
         </div>
+        {formErrors.Capital && <span className={style.error}>{formErrors.Capital}</span>}
+        <div className={style.input}>
+          <label htmlFor="emailLabel" className={style.label}>
+            Email:
+          </label>
+          <input type="email" name="Email" id="emailLabel"/>
+        </div>
+        {formErrors.Email && <span className={style.error}>{formErrors.Email}</span>}
         <div className={style.input}>
           <label htmlFor="addressLabel" className={style.label}>
             Address:
@@ -72,14 +149,16 @@ const EditCompany = () => {
           <label htmlFor="phoneNumberLabel" className={style.label}>
             Phone:
           </label>
-          <input type="text" name="Phone Number" id="phoneNumberLabel"/>
+          <input type="tel" name="PhoneNumber" id="phoneNumberLabel"/>
         </div>
+        {formErrors.PhoneNumber && <span className={style.error}>{formErrors.PhoneNumber}</span>}
         <div className={style.input}>
           <label htmlFor="websiteLabel" className={style.label}>
             Website:
           </label>
           <input type="text" name="Website" id="websiteLabel"/>
         </div>
+        {formErrors.Website && <span className={style.error}>{formErrors.Website}</span>}
         <div className={style.input}>
           <label className={style.label}>Logo:</label>
           <label htmlFor="logoUpload" className={style.uploadButton}>Insert Logo</label>
@@ -92,13 +171,12 @@ const EditCompany = () => {
               const file = event.target.files[0];
               // Implement multer logic to upload the file
             }}
-            required
           />
         </div>
         <div className={style.buttons}>
           <button type="reset" className={style.reset}>Reset</button>
           <button type="submit" className={style.submit}>
-            Update Info!
+            Update Info
           </button>
         </div>
       </form> 
