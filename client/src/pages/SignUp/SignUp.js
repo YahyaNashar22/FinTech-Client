a916ae64688727a11e6ab46fb2913d5f886fa3e7
YahyaNashar22 <cenations.people@live.com>
2023-12-02 07:20:47 +0200

    fixed sign u form structure/added img upload and axios post method

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -6,45 +6,152 @@ import {
   Select,
   MenuItem,
   Grid,
-  FormControl,
   Typography,
+  Input,
+  FormControl,
 } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Link, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import axios from "axios";
+import logo from "../../assets/Vectorlogo.png";
+import open from "../../assets/open.png";
+import closed from "../../assets/close.png";
 
 const SignUp = () => {
   const [pass, setPass] = useState(true);
+  const [file, setFile] = useState();
+  const [role, setRole] = useState("accountant");
+  const [isPending, setIsPending] = useState(false);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  /* used to handle show/hide password */
+
   let see = "";
+  let openClose = "";
   if (pass) {
     see = "password";
+    openClose = closed;
   } else {
     see = "text";
+    openClose = open;
   }
   const passHandler = () => {
     setPass(!pass);
   };
 
+  /* used to upload pictures */
+
+  const handlePictureChange = (e) => {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
+
+  /* used to handle the select role feature */
+
+  const handleRole = (e) => {
+    setRole(e.target.value);
+  };
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  /* used to handle the post request */
+
+  const notify = () => toast("Account created successfully !");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsPending(true);
+    axios
+      .post("http://localhost:5000/users/create", {
+        Name: name,
+        Email: email,
+        Password: pass,
+        Role: role,
+        Picture: file,
+      })
+      .then((res) => {
+        console.log(res);
+        setIsPending(false);
+        notify();
+        navigate("/");
+      });
+  };
   return (
     <>
-      <section className={style.wrapper}>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <motion.section
+        className={style.wrapper}
+        initial={{ x: -200, opacity: 0 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          ease: "easeOut",
+          duration: 0.5,
+        }}
+      >
         <aside className={style.left}>
-          <Typography variant="h1" component="h1">
+          <Typography
+            variant="h1"
+            component="h1"
+            sx={{ fontSize: "32px" }}
+            className={style.companyName}
+          >
+            <span className={style.companyLogo}>
+              <img src={logo} height="30px" width="30px" alt="company logo" />{" "}
+              {/* needs to be changed when we fetch */}
+            </span>
             Company Name {/* needs to be changed when we fetch */}
           </Typography>
-          <Typography variant="h2" component="h2">
-            Empower your team , sign up now !
+          <Typography
+            variant="h2"
+            component="h2"
+            sx={{ fontSize: "48px" }}
+            className={style.subtitle}
+          >
+            Empower Your Team , Sign Up Now !
           </Typography>
-          <FormControl className={style.signupForm}>
+          <Typography variant="p" component="p" className={style.fillForm}>
+            Fill the form below to create an account
+          </Typography>
+          <form onSubmit={handleSubmit} className={style.signupForm}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
                   sx={{
-                    "& input::placeholder": { color: "var(--secondery-green)" },
-                    "& label": { color: "var(--secondery-green)" },
+                    "& input::placeholder": { color: "var(--grey-color)" },
+                    "& label": { color: "var(--grey-color)" },
                     "& .MuiInputBase-input": {
-                      color: "var(--primary-green)",
+                      color: "var(--main-background-color)",
                     },
-                    "& fieldset": { borderColor: "var(--secondery-green)" },
+                    "& fieldset": { borderColor: "var(--grey-color)" },
                   }}
                   label="Name"
+                  onChange={handleName}
+                  value={name}
                   placeholder="Enter your Name"
                   variant="outlined"
                   type="text"
@@ -53,21 +160,19 @@ const SignUp = () => {
                   className={style.signupInp}
                 />
               </Grid>
-
               <Grid item xs={12}>
                 <Select
-                  labelId="select-label"
-                  label="Role"
+                  value={role}
+                  onChange={handleRole}
                   required
                   fullWidth
                   className={style.selection}
                   sx={{
-                    color: "var(--primary-green)",
-                    "& label": { color: "var(--secondery-green)" },
+                    color: "var(--main-background-color)",
                     "& input": {
-                      color: "var(--primary-green)",
+                      color: "var(--main-background-color)",
                     },
-                    "& fieldset": { borderColor: "var(--secondery-green)" },
+                    "& fieldset": { borderColor: "var(--grey-color)" },
                   }}
                 >
                   <MenuItem value="admin">Admin</MenuItem>
@@ -80,14 +185,16 @@ const SignUp = () => {
               <Grid item xs={12}>
                 <TextField
                   sx={{
-                    "& input::placeholder": { color: "var(--secondery-green)" },
-                    "& label": { color: "var(--secondery-green)" },
+                    "& input::placeholder": { color: "var(--grey-color)" },
+                    "& label": { color: "var(--grey-color)" },
                     "& .MuiInputBase-input": {
-                      color: "var(--primary-green)",
+                      color: "var(--main-background-color)",
                     },
-                    "& fieldset": { borderColor: "var(--secondery-green)" },
+                    "& fieldset": { borderColor: "var(--grey-color)" },
                   }}
                   label="Email"
+                  value={email}
+                  onChange={handleEmail}
                   placeholder="Enter your Email"
                   variant="outlined"
                   type="email"
@@ -96,28 +203,80 @@ const SignUp = () => {
                   className={style.signupInp}
                 />
               </Grid>
+              <Grid item xs={12} className={style.passContainer}>
+                <FormControl fullWidth>
+                  <TextField
+                    sx={{
+                      "& input::placeholder": { color: "var(--grey-color)" },
+                      "& label": { color: "var(--grey-color)" },
+                      "& .MuiInputBase-input": {
+                        color: "var(--main-background-color)",
+                      },
+                      "& fieldset": { borderColor: "var(--grey-color)" },
+                    }}
+                    label="Password"
+                    value={password}
+                    onChange={handlePassword}
+                    placeholder="Enter your Password"
+                    variant="outlined"
+                    type={see}
+                    required
+                    fullWidth
+                    className={style.passInp}
+                  />
+                  <span className={style.passEye} onClick={passHandler}>
+                    <img
+                      src={openClose}
+                      height="20px"
+                      width="20px"
+                      alt="open close eye"
+                    />
+                  </span>
+                </FormControl>
+              </Grid>
               <Grid item xs={12}>
-                <TextField
+                <Typography variant="p" component="p" className={style.imgTxt}>
+                  Kindly Upload Your Image
+                </Typography>
+                <Input
+                  label="Upload Your Image"
+                  type="file"
+                  onChange={handlePictureChange}
                   sx={{
-                    "& input::placeholder": { color: "var(--secondery-green)" },
-                    "& label": { color: "var(--secondery-green)" },
-                    "& .MuiInputBase-input": {
-                      color: "var(--primary-green)",
+                    width: "100%",
+                    padding: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "5px",
+                    fontSize: "16px",
+                    "&:focus": {
+                      outline: "none",
+                      borderColor: "var(--main-background-color)",
                     },
-                    "& fieldset": { borderColor: "var(--secondery-green)" },
                   }}
-                  label="Password"
-                  placeholder="Enter your Password"
-                  variant="outlined"
-                  type={see}
-                  required
-                  fullWidth
-                  className={style.signupInp}
                 />
               </Grid>
             </Grid>
-          </FormControl>
-          <button onClick={passHandler}>try me</button>
+
+            {isPending && (
+              <button disabled className={style.pendingSubmit}>
+                SIGN UP
+              </button>
+            )}
+            {!isPending && <button className={style.submitter}>SIGN UP</button>}
+            <Typography
+              variant="p"
+              component="p"
+              sx={{
+                color: "var(--grey-color)",
+                textAlign: "center",
+              }}
+            >
+              Already have an account ?
+              <Link to="/signin" className={style.signIn}>
+                Sign in
+              </Link>
+            </Typography>
+          </form>
         </aside>
         <aside className={style.right}>
           <img
@@ -127,7 +286,7 @@ const SignUp = () => {
             alt="sigun up page"
           />
         </aside>
-      </section>
+      </motion.section>
     </>
   );
 };
