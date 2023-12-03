c664ffdccb4c290f3f48cb82f5869af7a44d8afd
souhad <souhadmoussa86@gmail.com>
2023-12-03 23:13:06 +0200

    3 component for the report page

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -1,9 +1,59 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS,LineElement,LinearScale,PointElement, Legend, Tooltip, TimeScale } from "chart.js";
+ import 'chartjs-adapter-date-fns'
+ChartJS.register(
+  LineElement,
+   Legend,
+   Tooltip, 
+   TimeScale,
+   LinearScale,
+   PointElement 
+  )
 
-function LineChart({ chartData }) {
-  return <Line data={chartData} />;
-}
+function LineChart() {
+  const data = {
+    labels: ['2022-11-02', '2022-11-03', '2022-11-04','2022-11-05'],
+    datasets: [{
+      label:'expenses',
+      data: [6, 3, 9,3.69],
+      backgroundColor:' #4DA192',
+      borderColor:'black',
+      tension:0.4,
+      
+    }
+  
+    ]
+  }
+  const options = {
+   scales:{
+    x:{
+     type:'time',
+     time:{
+      unit:'day'
+     },
+     y:{
+beginAtZero:true
+     }
+    }
+   }
+  }
+  return<div
+  style={
+    {
+      padding: '20px',
+      width: '600px',
+      height:'300px'
+    }}>
+  
+  <Line
+    data={data}
+    options={options}
+    >
 
+    </Line>
+    
+</div>
+  
+    }
 export default LineChart;
--- client/src/pages/Report/Report.js
+++ client/src/pages/Report/Report.js
@@ -1,74 +1,100 @@
-import React, { useState } from "react";
-import Chart from "react-apexcharts";
-import styles from "../../pages/Report/Report.module.css";
+// import React, { useState } from "react";
+// import Chart from "react-apexcharts";
+// import styles from "../../pages/Report/Report.module.css";
 
+// const Report = () => {
+//   const [expenseState, setExpenseState] = useState({
+//     categories: ["Groceries", "Rent", "Utilities", "Entertainment", "Dining"],
+//     amounts: [300, 1200, 150, 100, 250],
+//   });
+
+//   const [pieChartState, setPieChartState] = useState({
+//     options: {
+//       colors: ["#0C2626", "#0FC2C0", " #0C2626", "#03A09D", "#015958"],
+//       labels: expenseState.categories,
+//     },
+//     series: expenseState.amounts,
+//   });
+
+//   const [state, setState] = useState({
+//     options: {
+//       colors: ["#4DA192", "#14EBBE"],
+//       chart: {
+//         id: "basic-bar",
+//       },
+//       xaxis: {
+//         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+//       },
+//     },
+//     series: [
+//       {
+//         name: "Income",
+//         data: [30, 40, 45, 50, 49, 60, 70, 91],
+//       },
+//       {
+//         name: "Outcome",
+//         data: [3, 60, 35, 80, 49, 70, 20, 81],
+//       },
+//     ],
+//   });
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.chartContainer}>
+//         <Chart
+//           options={state.options}
+//           series={state.series}
+//           type="bar"
+//           width="100%"
+//           height="600"
+//         />
+//       </div>
+//       <div className={styles.lineChartContainer}>
+//         <Chart
+//           options={state.options}
+//           series={state.series}
+//           type="line"
+//           width="100%"
+//           height="400"
+//         />
+//         <div className={styles.pieChartContainer}>
+//           <Chart
+//             options={pieChartState.options}
+//             series={pieChartState.series}
+//             type="pie"
+//             width="100%"
+//             height="100%"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Report;
+
+// Import necessary dependencies
+import React from 'react';
+import BarChart from '../../components/BarChart.js'; 
+import PieChart from '../../components/PieChart.js';
+import LineChart from '../../components/LineChart.js';
 const Report = () => {
-  const [expenseState, setExpenseState] = useState({
-    categories: ["Groceries", "Rent", "Utilities", "Entertainment", "Dining"],
-    amounts: [300, 1200, 150, 100, 250],
-  });
-
-  const [pieChartState, setPieChartState] = useState({
-    options: {
-      colors: ["#0C2626", "#0FC2C0", " #0C2626", "#03A09D", "#015958"],
-      labels: expenseState.categories,
-    },
-    series: expenseState.amounts,
-  });
-
-  const [state, setState] = useState({
-    options: {
-      colors: ["#4DA192", "#14EBBE"],
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-      },
-    },
-    series: [
-      {
-        name: "Income",
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-      {
-        name: "Outcome",
-        data: [3, 60, 35, 80, 49, 70, 20, 81],
-      },
-    ],
-  });
 
   return (
-    <div className={styles.container}>
-      <div className={styles.chartContainer}>
-        <Chart
-          options={state.options}
-          series={state.series}
-          type="bar"
-          width="100%"
-          height="600"
-        />
-      </div>
-      <div className={styles.lineChartContainer}>
-        <Chart
-          options={state.options}
-          series={state.series}
-          type="line"
-          width="100%"
-          height="400"
-        />
-        <div className={styles.pieChartContainer}>
-          <Chart
-            options={pieChartState.options}
-            series={pieChartState.series}
-            type="pie"
-            width="100%"
-            height="100%"
-          />
-        </div>
-      </div>
+    <div>
+    <div>
+      <BarChart />
+   
     </div>
+    <div>
+    <PieChart/>
+</div>
+<div>
+    <LineChart/>
+</div>
+    </div >
+
   );
-};
+}
 
 export default Report;
