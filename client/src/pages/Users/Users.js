77884ffdb4ccefc84302cfd2cf1243172130a6f8
anwar khawli <anwarkhawle@gmail.com>
2023-12-06 09:54:38 +0200

    start fetch-users-page

--- client/src/pages/Users/Users.js
+++ client/src/pages/Users/Users.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from './Users.module.css';
 import { DataTable } from "../../components/dataTable/dataTable";
+import axios from 'axios'
 const Users = () => {
+
+  let [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get("http://localhost:5000/users/getAll")
+      setUsers(response.data)
+    } catch (err) {
+      console.log("Error fetching users:", err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  console.log("user", users)
+
+
+
+
   return (
     <main className={style.main}>
       <div className={style.info}>
         <h1>Users</h1>
         <button className={style.button}>Add new User</button>
       </div>
-      <DataTable />
+      <DataTable
+        usersData={users}
+      />
     </main>
   )
 };
