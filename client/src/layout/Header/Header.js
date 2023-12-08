e30b0dfc569a8060957ee68411ba84e295ac2ef7
YahyaNashar22 <cenations.people@live.com>
2023-12-08 13:32:30 +0200

    finished login context and multer

--- client/src/layout/Header/Header.js
+++ client/src/layout/Header/Header.js
@@ -1,24 +1,45 @@
 // Header.js
-import React from 'react'
-import 
- {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
- from 'react-icons/bs'
- import  styles from "./Header.module.css";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
+import styles from "./Header.module.css";
+import axios from "axios";
+import userContext from "../../AuthContext";
 
-function Header({OpenSidebar}) {
+function Header({ OpenSidebar }) {
+  const [picture, setPicture] = useState("");
+  const { user } = useContext(userContext);
+  console.log(user);
+  useEffect(() => {
+    // axios.get(`http://localhost:5000/users/30`).then((res) => {
+    //   console.log(res);
+
+    setPicture(user.data.Picture);
+    console.log(`this is a ${picture}`);
+  }, []);
   return (
     <header className={styles.header}>
-        <div className={styles.menuIcon}>
-            <BsJustify className={styles.icon} onClick={OpenSidebar}/>
-        </div>
-        <div className={styles.headerLeft}>
-        </div>
-        <div className={ styles.headerRight}>
-            <BsFillBellFill className={styles.icon}/>
-            <BsPersonCircle className={styles.icon}/>
-        </div>
+      <div className={styles.menuIcon}>
+        <BsJustify className={styles.icon} onClick={OpenSidebar} />
+      </div>
+      <div className={styles.headerLeft}></div>
+      <div className={styles.headerRight}>
+        <BsFillBellFill className={styles.icon1} />
+        <img
+          src={"http://localhost:5000/images/" + picture}
+          alt="profile"
+          height="20px"
+          width="20px"
+          className={styles.icon}
+        />
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
