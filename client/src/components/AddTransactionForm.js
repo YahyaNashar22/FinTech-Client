e064b2037c6f454d45e727b7300ed36fa87f76a7
YahyaNashar22 <cenations.people@live.com>
2023-12-09 12:31:56 +0200

    set up the socket io server

--- client/src/components/AddTransactionForm.js
+++ client/src/components/AddTransactionForm.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
+import React, { useState, useEffect } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:5000");
 
 const AddTransactionForm = ({ onClose, onAddTransaction }) => {
-  const [title, setTitle] = useState('');
-  const [type, setType] = useState('');
-  const [date, setDate] = useState('');
-  const [value, setValue] = useState('');
-  const[UserID,setUserID]=useState('');
-  const[CategoryID,setCategoryID]=useState('');
-
-
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [date, setDate] = useState("");
+  const [value, setValue] = useState("");
+  const [UserID, setUserID] = useState("");
+  const [CategoryID, setCategoryID] = useState("");
+  //////////////////////////socketIO//////////////////
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Transaction added" });
+  };
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
+  }, []);
+  ////////////////////////////socketIO////////////////////
   const handleAddClick = () => {
     // Validate form data (you may add more validation logic here)
 
@@ -38,94 +50,95 @@ const AddTransactionForm = ({ onClose, onAddTransaction }) => {
   };
 
   return (
-    <Dialog open={true} onClose={onClose}   >
-            <Paper sx={{ backgroundColor: ' #25282C', p: 2, color: '#FFFFFF' }}>
-
-      <DialogTitle sx={{ color: '#FFFFFF' }}>Add New Transaction</DialogTitle>
-      <DialogContent >
-        <TextField
-          label="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          fullWidth
-          margin="normal"
-          InputProps={{ style: { color: '#FFFFFF' } }}
-          InputLabelProps={{ style: { color: '#FFFFFF' } }}
-          sx={{ '& fieldset': { borderColor: '#CCCCCC' } }}
-        />
-
-          
-        
-        <TextField
-          label="Type"
-          value={type}
-          onChange={(e) => setType(e.target.value)}
-          fullWidth
-          margin="normal"
-          InputProps={{ style: { color: '#FFFFFF' } }}
-          InputLabelProps={{ style: { color: '#FFFFFF' } }}
-          sx={{ '& fieldset': { borderColor: '#CCCCCC' } }}
-        
-        />
-        <TextField
-          label="Date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          fullWidth
-          margin="normal"
-
-          InputProps={{ style: { color: '#FFFFFF' } }}
-            InputLabelProps={{ style: { color: '#FFFFFF' } }}
-            sx={{ '& fieldset': { borderColor: '#CCCCCC' } }}
-          />
-        <TextField
-          label="Value"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          fullWidth
-          margin="normal"
-          InputProps={{ style: { color: '#FFFFFF' } }}
-          InputLabelProps={{ style: { color: '#FFFFFF' } }}
-          sx={{ '& fieldset': { borderColor: '#CCCCCC' } }}
-        />
-        
+    <Dialog open={true} onClose={onClose}>
+      <Paper sx={{ backgroundColor: " #25282C", p: 2, color: "#FFFFFF" }}>
+        <DialogTitle sx={{ color: "#FFFFFF" }}>Add New Transaction</DialogTitle>
+        <DialogContent>
           <TextField
-          label="UserID"
-          value={UserID}
-          onChange={(e) => setUserID(e.target.value)}
-          fullWidth
-          margin="normal"
-          InputProps={{ style: { color: '#FFFFFF' } }}
-            InputLabelProps={{ style: { color: '#FFFFFF' } }}
-            sx={{ '& fieldset': { borderColor: '#CCCCCC' } }}
+            label="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{ style: { color: "#FFFFFF" } }}
+            InputLabelProps={{ style: { color: "#FFFFFF" } }}
+            sx={{ "& fieldset": { borderColor: "#CCCCCC" } }}
+          />
+
+          <TextField
+            label="Type"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{ style: { color: "#FFFFFF" } }}
+            InputLabelProps={{ style: { color: "#FFFFFF" } }}
+            sx={{ "& fieldset": { borderColor: "#CCCCCC" } }}
           />
           <TextField
-          label="CategoryID"
-          value={CategoryID}
-          onChange={(e) => setCategoryID(e.target.value)}
-          fullWidth
-          margin="normal"
-          InputProps={{ style: { color: '#FFFFFF' } }}
-          InputLabelProps={{ style: { color: '#FFFFFF' } }}
-          sx={{
-            '& fieldset': { borderColor: '#CCCCCC' },
-            '&:hover fieldset': { borderColor: '#CCCCCC' },
-            '&.Mui-focused fieldset': { borderColor: '#FF0000' }, // Red when focused
-          }}
-        />
-        
-        {/* Add more fields as needed */}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleAddClick} variant="contained" color="primary">
-          Add
-        </Button>
-        <Button onClick={onClose} color="primary">
-          Cancel
-        </Button>
-      </DialogActions>
+            label="Date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{ style: { color: "#FFFFFF" } }}
+            InputLabelProps={{ style: { color: "#FFFFFF" } }}
+            sx={{ "& fieldset": { borderColor: "#CCCCCC" } }}
+          />
+          <TextField
+            label="Value"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{ style: { color: "#FFFFFF" } }}
+            InputLabelProps={{ style: { color: "#FFFFFF" } }}
+            sx={{ "& fieldset": { borderColor: "#CCCCCC" } }}
+          />
+
+          <TextField
+            label="UserID"
+            value={UserID}
+            onChange={(e) => setUserID(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{ style: { color: "#FFFFFF" } }}
+            InputLabelProps={{ style: { color: "#FFFFFF" } }}
+            sx={{ "& fieldset": { borderColor: "#CCCCCC" } }}
+          />
+          <TextField
+            label="CategoryID"
+            value={CategoryID}
+            onChange={(e) => setCategoryID(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{ style: { color: "#FFFFFF" } }}
+            InputLabelProps={{ style: { color: "#FFFFFF" } }}
+            sx={{
+              "& fieldset": { borderColor: "#CCCCCC" },
+              "&:hover fieldset": { borderColor: "#CCCCCC" },
+              "&.Mui-focused fieldset": { borderColor: "#FF0000" }, // Red when focused
+            }}
+          />
+
+          {/* Add more fields as needed */}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              handleAddClick();
+              sendMessage();
+            }}
+            variant="contained"
+            color="primary"
+          >
+            Add
+          </Button>
+          <Button onClick={onClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
       </Paper>
-
     </Dialog>
   );
 };
